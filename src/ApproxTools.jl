347e9f0a0ae806aae9052ead3fe0104197c6c1a1
Simon Etter <s.etter@warwick.ac.uk>
2018-03-17 15:05:27 +0000

    Load Compat package for ntuple(f,Val{K}()) syntax.

--- src/ApproxTools.jl
+++ src/ApproxTools.jl
@@ -3,5 +3,6 @@ module ApproxTools
 include("base.jl");      export interpolate, map2refinterval
 include("bary.jl");      export geometric_mean_distance,baryweights,bary,Barycentric
 include("chebyshev.jl"); export chebpoints, chebcoeffs, chebeval
+using Compat
 
 end # module
