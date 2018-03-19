37169485f0e4588d514bc709701d529c44910926
Simon Etter <s.etter@warwick.ac.uk>
2018-03-19 11:53:50 +0000

    Restart from clean slate

--- src/ApproxTools.jl
+++ src/ApproxTools.jl
@@ -1,11 +1,6 @@
 module ApproxTools
 
+# For ntuple(f,Val{K}()) syntax
 using Compat
-
-include("utils.jl");           export EmptyArray, EmptyVector, EmptyMatrix, map2refinterval
-
-include("base.jl");            export interpolate
-include("bary.jl");            export baryweights, bary, Barycentric
-include("chebyshev.jl");       export chebpoints, chebcoeffs, chebeval
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,2 @@
 using ApproxTools
 using Base.Test
-
-const BitsFloats = (Float32,Float64)
-const Floats = (BitsFloats..., BigFloat)
-
-srand(42)
-
-# Dummy rng for BigFloats
-myrand(args...) = rand(args...)
-myrand(::Type{BigFloat}, args...) = big.(rand(Float64,args...)) .+ eps(Float64)*big.(rand(Float64,args...))
-myrand(::Type{Complex{BigFloat}}, args...) = big.(rand(Complex128,args...)) .+ eps(Float64)*big.(rand(Complex128,args...))
-
-include("base.jl")
-include("bary.jl")
-include("chebyshev.jl")
-include("utils.jl")
