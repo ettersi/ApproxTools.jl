8341bb781206aa7708fd8158f8c86853bc39b045
Simon Etter <s.etter@warwick.ac.uk>
2018-03-21 18:39:56 +0000

    Add type stable `promote_type` overload

--- src/ApproxTools.jl
+++ src/ApproxTools.jl
@@ -2,6 +2,12 @@ module ApproxTools
 
 using Compat
 
+if VERSION < v"0.7-"
+    # promote_type with four or more arguments is not type stable on 0.6.2
+    Base.promote_type(A,B,C,D=Union{},E=Union{},F=Union{},G=Union{},H=Union{}) =
+        promote_type(promote_type(promote_type(promote_type(promote_type(promote_type(promote_type(A,B),C),D),E),F),G),H)
+end
+
 include("tensor.jl")
 export tucker, cartesian, tensor
 
