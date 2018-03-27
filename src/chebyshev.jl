a6ebf82e5c9d7fdd07f81845bdb2463e3daee261
Simon Etter <s.etter@warwick.ac.uk>
2018-03-27 22:53:22 +0200

    Cache chebpoints instead of computing on the fly

--- src/chebyshev.jl
+++ src/chebyshev.jl
@@ -6,19 +6,21 @@ The `n` Chebyshev points on [-1,1] in type `T`.
 chebpoints(n::Int) = chebpoints(Float64,n)
 chebpoints(T::Type{<:AbstractFloat}, n::Int) = Chebpoints{T}(n)
 struct Chebpoints{T} <: AbstractVector{T}
-    n::Int
-    m::T
-    b::T
+    data::Vector{T}
     function Chebpoints{T}(n) where {T}
         if n == 1
-            return new(n, T(0), T(π)/2)
+            return new(T[0])
         else
-            return new(n, -T(π)/(n-1), T(π)*n/(n-1))
+            data = Vector{T}(n)
+            for i = 1:n
+                data[i] = cos(T(π)*(n-i)/(n-1))
+            end
+            return new(data)
         end
     end
 end
-Base.size(x::Chebpoints) = (x.n,)
-Base.getindex(x::Chebpoints, i::Int) = cos(x.m*i+x.b)
+Base.size(x::Chebpoints) = size(x.data)
+Base.getindex(x::Chebpoints, i::Int) = x.data[i]
 
 
 function baryweights(x::Chebpoints)
