0f195cc9e47f6ab1a08163147b7c688873887241
Simon Etter <s.etter@warwick.ac.uk>
2018-08-08 09:24:14 +0800

    Document fnorm

--- src/fnorm.jl
+++ src/fnorm.jl
@@ -17,6 +17,39 @@ end
 Base.@pure puremax(a,b) = a > b ? a : b
 Base.@pure fndims(f,g) = puremax(fndims(f), fndims(g))
 
+"""
+    fnorm(f, ntest=101)
+    fnorm(T, f, ntest=101)
+
+Lower-bound the supremum norm of `f` on `[-1,1]^d` by the maximum value
+of `abs(f)` on the Cartesian grid of `ntest` equally spaced points in
+each dimension.
+
+`ntest` can be either an integer or a tuple of integers. If provided,
+`T` specifies the type to be used for the grid points (default `Float64`)
+
+# Examples
+
+```
+julia> fnorm(exp)
+2.718281828459045
+
+julia> fnorm(Float32, exp)
+2.7182817f0
+
+julia> fnorm(x->sin(3x))
+0.9999417202299663
+
+julia> fnorm(x->sin(3x),10000)
+0.9999999903031747
+
+julia> fnorm( (x1,x2) -> 1/(x1+x2+1/3+im) )
+0.999977778518491
+
+julia> fnorm( (x1,x2) -> 1/(x1+x2+1/3+im), (1000,1000) )
+0.9999994989988744
+```
+"""
 fnorm(args...) = fnorm(Float64, args...)
 function fnorm(
     ::Type{T},
