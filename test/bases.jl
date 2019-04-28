4f498518876a6c6953ad61911386cd7c90e94965
ettersi <s.etter@warwick.ac.uk>
2019-04-28 15:34:02 +0800

    Rework basis testing

--- test/bases.jl
+++ test/bases.jl
@@ -1,82 +1,134 @@
-function test_evaluation(B, fun)
-    if length(B) == 0
-        @testset "evaluation" begin
-            @test length(B) == 0
-            @test iterate(B|42) == nothing
-        end
-    else
-        @testset "evaluation, #points = $n" for n = 1:5
-            x = n > 1 ? LinRange(-1,1,n) : LinRange(0,0,1)
-            M = Diagonal(collect(x))
-            v = ones(n)
-            vals = (p->p.(x)).(fun)
+function test_empty(B)
+    @test length(B) == 0
+    @test iterate(B|42) == nothing
+end
 
-            @test @inferred(Matrix(B,x)) ≈ hcat(vals...)
-            @test all(@inferred(collect(B|M)) .≈ Diagonal.(vals))
-            @test all(@inferred(collect(B|(M,v))) .≈ vals)
+unit(n,i) = [i == j for j = 1:n]
 
-            for i = 1:length(B)
-                @test B[i].(x) ≈ vals[i]
-                @test B[i](M) ≈ Diagonal(vals[i])
-                @test B[i](M,v) ≈ vals[i]
-            end
-        end
+matrix_reference(fun,x) = [fj(xi) for xi in x, fj in fun]
+matrix_matrix(B,x) = Matrix(B,x)
+matrix_iterate(B,x) = invoke(Matrix, Tuple{ApproxTools.AbstractBasis,AbstractVector}, B,x)
+matrix_getindex(B,x) = [B[j](xi) for xi in x, j in 1:length(B)]
+matrix_evaltransform_1D(B,x) = hcat((ApproxTools.evaltransform(B,x,unit(length(B),i)) for i = 1:length(B))...)
+matrix_evaltransform_2D(B,x) = ApproxTools.evaltransform(B,x,Matrix(I,(length(B),length(B))))
+
+function test_values(B,fun,args...)
+    ref = matrix_reference(fun,args...)
+    @test matrix_matrix(B,args...) ≈ ref
+    @test matrix_iterate(B,args...) ≈ ref
+    @test matrix_getindex(B,args...) ≈ ref
+    @test matrix_evaltransform_1D(B,args...) ≈ ref
+    @test matrix_evaltransform_2D(B,args...) ≈ ref
+end
+
+function test_matrix_eval(B,x)
+    ref = Matrix(B,x)
+    M = Diagonal(collect(x))
+    for (i,Mi) in enumerate(B|M)
+        @test diag(Mi) ≈ ref[:,i]
+        @test diag(B[i](M)) ≈ ref[:,i]
+    end
+end
+
+function test_matvec_eval(B,x)
+    ref = Matrix(B,x)
+    M = Diagonal(collect(x))
+    v = ones(length(x))
+    for (i,vi) in enumerate(B|(M,v))
+        @test vi ≈ ref[:,i]
+        @test B[i]((M,v)) ≈ ref[:,i]
     end
 end
 
 function test_approximation(B, fun)
-    length(B) == 0 && return
-
     n = length(B)
-    unit = i->[i == j for j = 1:n]
-    x = n > 1 ? LinRange(-1,1,n) : LinRange(0,0,1)
-
-    @testset "approximation, i = $i" for i = 1:n
-        @test coeffs(approximate(fun[i],B)) ≈ unit(i)
-        @test coeffs(approximate(fun[i],(x,B))) ≈ unit(i)
+    for i = 1:n
+        @test coeffs(approximate(fun[i],B)) ≈ unit(n,i)
     end
 end
 
-monomials = [one,identity,x->x^2,x->x^3,x->x^4]
-chebpolys = [one, identity, x->2x^2-1, x->4x^3-3x, x->8x^4-8x^2+1]
-exp_approx = approximate(exp, Monomials(5))
+
 
 @testset "Monomials" begin
-    @testset "degree = $n" for n = 0:length(monomials)
-        test_evaluation(Monomials(n), monomials[1:n])
-        test_approximation(Monomials(n), monomials[1:n])
+    fun = [one,identity,x->x^2,x->x^3,x->x^4]
+    B = n->Monomials(n)
+
+    test_empty(B(0))
+    @testset for nB = 1:length(fun)
+        @testset for nx = 1:5
+            test_values(B(nB), fun[1:nB], TrapezoidalPoints(nx))
+            test_matrix_eval(B(nB), TrapezoidalPoints(nx))
+            test_matvec_eval(B(nB), TrapezoidalPoints(nx))
+        end
+        test_approximation(B(nB), fun[1:nB])
     end
 end
 
 @testset "Chebyshev" begin
-    @testset "degree = $n" for n = 0:length(chebpolys)
-        test_evaluation(Chebyshev(n), chebpolys[1:n])
-        test_approximation(Chebyshev(n), chebpolys[1:n])
+    fun = [one, identity, x->2x^2-1, x->4x^3-3x, x->8x^4-8x^2+1]
+    B = n->Chebyshev(n)
+
+    test_empty(B(0))
+    @testset for nB = 1:length(fun)
+        @testset for nx = 1:5
+            test_values(B(nB), fun[1:nB], TrapezoidalPoints(nx))
+            test_matrix_eval(B(nB), TrapezoidalPoints(nx))
+            test_matvec_eval(B(nB), TrapezoidalPoints(nx))
+        end
+        test_approximation(B(nB), fun[1:nB])
     end
 end
 
 @testset "Weighted" begin
-    @testset "degree = $n" for n = 0:length(monomials)
-        test_evaluation(Weighted(Monomials(n),exp_approx), [x->exp_approx(x)*p(x) for p in monomials[1:n]])
+    exp_approx = approximate(exp, Monomials(5))
+    monomials = [one,identity,x->x^2,x->x^3,x->x^4]
+    fun = [x->exp_approx(x)*p(x) for p in monomials]
+    B = n->Weighted(Monomials(n),exp_approx)
+
+    test_empty(B(0))
+    @testset for nB = 1:length(fun)
+        @testset for nx = 1:5
+            test_values(B(nB), fun[1:nB], TrapezoidalPoints(nx))
+            test_matrix_eval(B(nB), TrapezoidalPoints(nx))
+            test_matvec_eval(B(nB), TrapezoidalPoints(nx))
+        end
     end
 end
 
-z = [2,3,4]
-poles = [x->1/(x - z) for z in z]
 @testset "Poles" begin
-    @testset "degree = $n" for n = 0:length(poles)
-        test_evaluation(Poles(z[1:n]), poles[1:n])
+    z = [2,3,4]
+    fun = [x->1/(x - z) for z in z]
+    B = n->Poles(z[1:n])
+
+    test_empty(B(0))
+    @testset for nB = 1:length(fun)
+        @testset for nx = 1:5
+            test_values(B(nB), fun[1:nB], TrapezoidalPoints(nx))
+            test_matrix_eval(B(nB), TrapezoidalPoints(nx))
+            test_matvec_eval(B(nB), TrapezoidalPoints(nx))
+        end
     end
 end
 
 @testset "Basis" begin
-    x = LinRange(-1,1,11)
-    @test Matrix(Basis(monomials),x) ≈ [f(x) for x in x, f in monomials]
+    fun = (exp,sin,cos)
+    B = n->Basis(fun[1:n])
+
+    test_empty(B(0))
+    @testset for nB = 1:length(fun)
+        @testset for nx = 1:5
+            test_values(B(nB), fun[1:nB], TrapezoidalPoints(nx))
+        end
+    end
 end
 
 @testset "Combined" begin
-    test_evaluation( Combined( Monomials(length(monomials)), Chebyshev(length(chebpolys)) ), [monomials; chebpolys;] )
-    test_evaluation( Combined( Monomials(length(monomials)), Chebyshev(0) ), [monomials;] )
-    test_evaluation( Combined( Monomials(0), Chebyshev(length(chebpolys)) ), [chebpolys;] )
-    test_evaluation( Combined( Monomials(0), Chebyshev(0) ), [] )
+    monomials = [one,identity,x->x^2]
+    chebyshev = [one, identity, x->2x^2-1]
+    @testset for nx = 1:5
+        test_values(Combined(Monomials(3),Chebyshev(3)), [monomials;chebyshev;], TrapezoidalPoints(nx))
+        test_values(Combined(Monomials(0),Chebyshev(3)), [          chebyshev;], TrapezoidalPoints(nx))
+        test_values(Combined(Monomials(3),Chebyshev(0)), [monomials;          ], TrapezoidalPoints(nx))
+        test_empty(Combined(Monomials(0),Chebyshev(0)))
+    end
 end
